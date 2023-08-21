df4d201df4dd964200619eb8b477e7d884017700
Vaishnavi Kamdi <vkvaishnavi7kamdi@gmail.com>
2023-08-21 17:02:24 +0530

    Added divide by zero condition

--- client3.py
+++ client3.py
@@ -42,7 +42,9 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return price_a/price_b
+    if (price_b == 0):
+        return
+    return price_a / price_b
 
 
 # Main
